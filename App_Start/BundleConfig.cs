4d84aee314627ba93ceda5ac6f1b000b33ece6e3
James Keith Buchanan <jamesbuchanan007@gmail.com>
2021-05-11 17:45:08 -0500

    initial commit

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace AbyssAzul
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Content/html/vendor"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/Content/html/vendor/bootstrap"));
+
+            bundles.Add(new StyleBundle("~/Content/html/css").Include(
+                      "~/Content/html/vendor/bootstrap/css"));
+        }
+    }
+}
