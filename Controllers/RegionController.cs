35d2e4853e79762f498177be31ccad231c85fb97
James Keith Buchanan <jamesbuchanan007@gmail.com>
2021-05-16 15:28:26 -0500

    in progress

---
+++ Controllers/RegionController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AbyssAzul.Controllers
+{
+    public class RegionController : Controller
+    {
+        // GET: Region
+        public ActionResult EcuadorPeru()
+        {
+            return View();
+        }
+        public ActionResult BrazilUruguay()
+        {
+            return View();
+        }
+        public ActionResult GuyanaSurinam()
+        {
+            return View();
+        }
+        public ActionResult ShrimpCamaron()
+        {
+            return View();
+        }
+        public ActionResult OtherSpecialties()
+        {
+            return View();
+        }
+    }
+}
