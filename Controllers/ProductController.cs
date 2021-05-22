326d4b1580becf67aa25ca13f8b4d4200c8832c2
James Keith Buchanan <jamesbuchanan007@gmail.com>
2021-05-22 17:05:18 -0500

    in progress

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,18 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using AbyssAzul.Models;
+using Dapper;
 
 namespace AbyssAzul.Controllers
 {
     public class ProductController : Controller
     {
         // GET: Product
-        public ActionResult Product()
+        public ActionResult Product(string productId)
         {
-            //var product = new Product();
+            var product = new Product();
+            var isNum = int.TryParse(productId, out var intProductId);
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Default"].ConnectionString))
+            {
+                var result = conn.Query<Product>(sql: "Product_Get",
+                    commandType: CommandType.StoredProcedure, param: new
+                    {
+                       ProductId = intProductId
+                    });
+            }
 
             return View();
         }
